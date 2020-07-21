09e0818ed792da33b43137515ae895b47482ff77
Rafayel <rafayelarakelyan803080@gmail.com>
2020-07-22 03:34:20 +0400

    ready to be hosted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomePageComponent},
+  {path: 'about', component: AboutUsComponent},
+  {path: 'contacts', component: ContactUsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
